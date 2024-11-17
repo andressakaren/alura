cbd1b558e0b0f1ffb0ebce6ff578ee874457e3a5
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-11-17 15:43:06 -0300

    update

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,13 +1,24 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número de 1 a 10: ');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// condição
-if (chute == numeroSecreto) {
-    alert(`Isso ai, você descobriu o número secreto: ${numeroSecreto}!!!`);
-} else {
-    alert('Você errou!! :(');
+// Enquanto chute não igual ao numerosSecreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número de 1 a 10: ');
+    // condição
+    if (chute == numeroSecreto) {
+        alert(`Isso ai, você descobriu o número secreto: ${numeroSecreto}, com ${tentativas} tentativas.!!!`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que o ${chute}`);
+        } else {
+            alert(`O número secreto é maior que o ${chute}`);
+        }
+        tentativas++
+    }
+
 }
 
 
